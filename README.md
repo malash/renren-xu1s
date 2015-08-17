290ec4e3bc67d46fdeb3d3979bb6857f632d3eb9
Malash <i@malash.me>
2015-08-17 22:41:48 +0800

    version 1.0.0 ha?

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# renren-xu1s
+#Renren续一秒
+
+![蛤？](screenshot.png)
+
+##用途
+查看人人上被屏蔽的图片
+
+##原理
+码农的锅
+
+##用法
+
+前往[https://greasyfork.org/zh-CN/scripts/11803-renren-xu1s](https://greasyfork.org/zh-CN/scripts/11803-renren-xu1s)进行安装
---
+++ src/renren-xu1s.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name  Renren续一秒
+// @namespace https://malash.me/
+// @author  Malash <i@malash.me>
+// @icon  http://a.xnimg.cn/favicon.ico
+// @version 1.0.0
+// @description Renren续一秒——被屏蔽内容查看器
+// @homepageURL https://github.com/malash/renren-xu1s
+// @include http://www.renren.com/*
+// @grant none
+// ==/UserScript==
+
+(function ($, window) {
+    $('body').append('<style>.xu1s{opacity: 0.5;color: red;font-size: 1.2em;}.xu1s:hover{opacity: 1;color: red !important;text-shadow: 0 0 10px yellow;}.xu1s:before{content:"看不了？"}.xu1s:hover:before{content:"续一秒！"}</style>');
+    setInterval(function(){
+        $('.share-photo img').each(function() {
+            if ($(this).data('xu1s')) {
+                return;
+            }
+            console.log('new', this);
+            $(this).data('xu1s', 'xu1s');
+            $($(this).parent().parent()).children('.source-desc').append('<a href="' + $(this).data('viewer').url + '" class="xu1s" target="_blank"></a>')
+        });
+    }, 1000);
+})(jQuery, window);
